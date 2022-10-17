67fd6ba11cb8a4bb21ce2668f3ad80e08aa64401
paul-kelly-dit <36048467+paul-kelly-dit@users.noreply.github.com>
2022-10-17 07:15:12 +0000

    added nodejs

---
+++ nodejs/event-transmitter.js
@@ -0,0 +1,28 @@
+const EventEmitter = require('events');
+
+const myEmitter = new EventEmitter();
+
+resetPassword = function (){
+    console.log('Password has changed');
+};
+
+updateUIOnPasswordReset = function (){
+    console.log('UI - Password successfully updated');
+};
+
+handleError = function (errorCode){
+    console.log('Error handler');
+    console.log('Problem processing : '+ errorCode);
+
+};
+
+myEmitter.on('PASSWORD_RESET', resetPassword);
+myEmitter.on('PASSWORD_RESET', updateUIOnPasswordReset);
+// myEmitter.on('error', handleError);
+
+// myEmitter.emit('error', 9);
+
+
+
+
+myEmitter.emit('error', 9);
