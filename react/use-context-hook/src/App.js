f44d2ecc39008f0639d593b2584a4acd7fce60ee
paul-kelly-dit <36048467+paul-kelly-dit@users.noreply.github.com>
2022-11-01 12:03:11 +0000

    Update App.js

--- react/use-context-hook/src/App.js
+++ react/use-context-hook/src/App.js
@@ -1,17 +1,49 @@
 import React, { useContext } from 'react';
 
+// using useContext - we do not have to pass props to all the child components
+
 const AccountContext = React.createContext({});
 
 const ContextExample = () => {
-  const accountData = useContext(AccountContext);
 
   return (
     <div>
-      <p>Name: {accountData.name}</p>
-      <p>Role: {accountData.role}</p>
+      <p>Parent Component</p>
+      <ChildComponent />
     </div>
   );
 };
+
+function ChildComponent() {
+  return (
+    <div>
+      <h2>This is Child Component</h2>
+      <hr />
+      <SubChildComponent />
+    </div>
+  );
+}
+
+function SubChildComponent() {
+  return (
+    <div>
+      <h2>This is SubChild Component</h2>
+      <hr />
+      <SubSubChildComponent />
+    </div>
+  );
+}
+
+function SubSubChildComponent() {
+  var accountData = React.useContext(AccountContext);
+  return (
+    <div>
+      <h3>This is Sub Sub Child Component</h3>
+      <p>Name: {accountData.name}</p>
+      <p>Role: {accountData.role}</p>
+     </div>
+  );
+}
 
 const App = () => (
   <AccountContext.Provider value={{ name: 'Manuel', role: 'admin' }}>
